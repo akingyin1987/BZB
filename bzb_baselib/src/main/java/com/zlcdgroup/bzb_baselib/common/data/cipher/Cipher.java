0c7d0225d03605a89713c3725838b6f97ba338bc
akingyin1987 <akingyin@163.com>
2016-01-17 18:02:59 +0800

    Initial commit

---
+++ bzb_baselib/src/main/java/com/zlcdgroup/bzb_baselib/common/data/cipher/Cipher.java
@@ -0,0 +1,9 @@
+package com.zlcdgroup.bzb_baselib.common.data.cipher;
+
+/**
+ * @author MaTianyu
+ * @date 14-7-31
+ */
+public abstract class Cipher implements Encrypt,Decrypt{
+
+}
---
+++ bzb_baselib/src/main/java/com/zlcdgroup/bzb_baselib/common/data/cipher/Encrypt.java
@@ -0,0 +1,9 @@
+package com.zlcdgroup.bzb_baselib.common.data.cipher;
+
+/**
+ * @author MaTianyu
+ * @date 14-7-31
+ */
+public interface Encrypt {
+    public byte[] encrypt(byte[] res);
+}
